6e2652b5448c3795b3eb25f495533b32e57bbf98
Ajax12345 <jpetullo14@gmail.com>
2021-04-16 14:32:39 -0400

    updates to grammar

--- caspian/src/caspian_grammar.py
+++ caspian/src/caspian_grammar.py
@@ -41,6 +41,7 @@ grammar = [
     (Token.Bool,    Token.Label.match('true')
                     |Token.Label.match('false')),
     (Token.Null,    Token.Label.match('null')),
+    (Token.In, Token.Label.match('in')),
     (Token.Operator, Token.Plus
                     |Token.Minus
                     |Token.Star
@@ -54,7 +55,7 @@ grammar = [
                     |(Token.Not&Token.Eq)._('not_equals')
                     |Token.Label.match('and')._('bool_and')
                     |Token.Label.match('or')._('bool_or')
-                    |Token.Label.match('in')._('in')),
+                    |Token.In),
     (Token.Operation, (Token.Expr&Token.Operator&Token.Expr).neg_lookahead(Token.Star&Token.Slash)),
     (Token.NegativeVal, Token.Minus&Token.Expr),
     (Token.Comment,   Token.Slash&Token.Slash),
@@ -84,6 +85,18 @@ grammar = [
                       |Token.Expr&Token.Pipe&Token.RArrow&Token.ChainCall).ml),
     (Token.Getattr, Token.Expr&Token.Dot&Token.Expr),
     (Token.Getitem, Token.Expr&Token.OBracket&Token.Expr&Token.CBracket),
+    (Token.If,      Token.Label.match('if')),
+    (Token.Elif,    Token.Label.match('elif')),
+    (Token.Else,    Token.Label.match('else')),
+    (Token.For,     Token.Label.match('for')),
+    (Token.Yield,   Token.Label.match('yield')),
+    (Token.YieldFrom,   Token.Yield&Token.Label.match('from')),
+    (Token.Async,   Token.Label.match('async')),
+    (Token.AsyncFor,  Token.Async&Token.For),
+    (Token.Await,   Token.Label.match('await')),
+    (Token.ForExpr, (Token.For|Token.AsyncFor)&Token.CommaList&Token.In&Token.Expr),
+    (Token.ForBlockInline, Token.ForExpr&Token.ForExpr
+                           |Token.ForExpr&Token.ForBlockInline),
     (Token.Expr,    Token.Label
                     |Token.Operation
                     |Token.Integer
@@ -95,11 +108,16 @@ grammar = [
                     |Token.Map
                     |(Token.OParen&Token.Expr&Token.CParen)._('paren_group')
                     |Token.Primative
-                    |Token.Expr&Token.OParen&Token.CParen._('f_call')
-                    |Token.Expr&Token.OParen&Token.CommaList&Token.CParen._('f_call')
+                    |(Token.Expr&Token.OParen&Token.CParen)._('f_call')
+                    |(Token.Expr&Token.OParen&Token.CommaList&Token.CParen)._('f_call')
                     |Token.ChainCall
                     |Token.Getattr
                     |Token.Getitem
+                    |(Token.Expr&Token.If&Token.Expr&Token.Else&Token.Expr)._('inline_conditional')
+                    |(Token.Await&Token.Expr)._('await'),
+                    |Token.OBracket&Token.Expr&(Token.ForExpr|Token.ForBlockInline)&Token.CBracket)._('array_comp')
+                    |Token.OBracket&Token.Expr&(Token.ForExpr|Token.ForBlockInline)&Token.IfCond&Token.CBracket)._('array_comp_conditional')
+                    
                     ),
 
     (Token.Assign, (Token.Label
@@ -111,12 +129,16 @@ grammar = [
                     &Token.Eq
                     &(Token.Expr
                     |Token.CommaList)),
+    (Token.IfCond, Token.If&Token.Expr),
+    (Token.ElifCond, Token.Elif&Token.Expr),
     
 
 ]
-#TODO: inline conditional
-#TODO: array comprehension
-#TODO: map comprehension
-#TODO: await <promise>
+#Expr TODO: array comprehension
+#Expr TODO: map comprehension
+#TODO: need key-value pair (see below)
+#TODO: merge Token.CommaList and Token.MapCommaList and update rest of grammar accordingly
+
+
 if __name__ == '__main__':
-    print(grammar[6][-1])
+    print(grammar[-2][-1])
