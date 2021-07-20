c2468686d081b744037f8f1001b3808352de31a3
Ajax12345 <jpetullo14@gmail.com>
2021-07-20 10:55:43 -0400

    integrated parser

--- caspian/src/caspian_compile.py
+++ caspian/src/caspian_compile.py
@@ -1,5 +1,6 @@
 import typing, caspian_parser, state_objects
-import collections, itertools
+import collections, itertools, re, os
+import internal_errors
 
 class CallStack:
     def __init__(self, max_depth = 1000) -> None:
@@ -22,12 +23,18 @@ class CaspianCompile:
 
     def compile(self, resource:str, f:bool=True) -> None:
         if f:
+            if not os.path.isfile(resource):
+                raise internal_errors.InvalidSource(f"'{resource}' is not a file")
+            
             self.call_stack.head = state_objects.FileLevel(resource)
             with open(resource) as f:
                 resource = f.read()
 
         with caspian_parser.Parser(self) as p, caspian_parser.ASTGen(self) as astgen:
-            pass
+            lines, _r_obj = p.parse(resource)
+            astgen.input_lines = lines
+            ast = astgen.create_ast(_r_obj)
+            print('resulting ast from compiler', ast)
 
     
     def __exit__(self, *_) -> None:
@@ -35,5 +42,6 @@ class CaspianCompile:
     
 
 if __name__ == '__main__':
-    pass
+    with CaspianCompile() as c:
+        c.compile('testing_file.txt')
 
---
+++ caspian/src/internal_errors.py
@@ -0,0 +1,2 @@
+class InvalidSource(Exception):
+    pass
