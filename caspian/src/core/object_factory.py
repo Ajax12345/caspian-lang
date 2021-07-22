74395a89e9c0ae8a45fee89072141457ddf1da31
Ajax12345 <jpetullo14@gmail.com>
2021-07-22 11:50:52 -0400

    start of core

---
+++ caspian/src/core/object_factory.py
@@ -0,0 +1,67 @@
+import typing, state_objects
+
+class CaspianObjFactor:
+    '''
+    caspian object model
+    ---------------------
+    ObjParent:
+        public:
+            @attr __name__
+        primative
+            @method toString
+            @method getitem
+
+    Obj inherits ObjParent:
+        public:
+            @attr __name__
+            @attr __identity___ 
+            @attr __parent_count__
+            @attr __id__
+        primative:
+            @method call
+        bindings: //attributes that become bound to an instance of Obj
+            public:
+                //
+            primative:
+                //
+        states:
+            //stores particulars about how the Obj is executed
+            on_exec:
+                //the actual callable block, either a caspian ast or a Python callable
+        @int ref_count //how many pointers to the object
+        @list parents //stores the 
+
+    '''
+    def __init__(self, _heap:typing.Union['MemHeap', None] = None) -> None:
+        self.heap = _heap
+        self.name_bindings = {}
+
+    @property
+    def fun(self) -> typing.Callable:
+        class _fun:
+            def generator(self, _f:typing.Callable) -> typing.Callable:
+                return _f
+
+            def __call__(self, _f:typing.Callable) -> typing.Callable:
+                return _f
+
+        return _fun()
+        
+    @property
+    def primative(self) -> typing.Callable:
+        class _primative:
+            def getitem(self, _f:typing.Callable) -> typing.Callable:
+
+                return _f
+
+        return _primative()
+
+    def base_class(self, _f:typing.Callable) -> typing.Callable:
+        return _f
+
+    def class_(self, _f:typing.Callable) -> typing.Callable:
+        return _f
+
+    def __call__(self, _heap:'MemHeap') -> 'CaspianObjFactor':
+        self.heap = _heap
+        return self
